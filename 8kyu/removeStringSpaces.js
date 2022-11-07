5af25d01dfe84c870beb46681b6b857f4ac169eb
Mika Chiusiwa <mikachiusiwa@gmail.com>
2022-11-07 19:54:45 +0900

    Add removeStringSpaces

---
+++ 8kyu/removeStringSpaces.js
@@ -0,0 +1,8 @@
+// Simple, remove the spaces from the string, then return the resultant string.
+
+// Answer
+function noSpace(x){
+    let noSpaceString = x.split(' ').join('')
+    
+    return noSpaceString
+  }
